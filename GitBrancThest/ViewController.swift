abcb02c1492be55ccc8ad0b08b2c36c0096e1a4b
hesenovveli <velihesenov.001@gmail.com>
2024-08-16 19:50:02 +0400

    add number

--- GitBrancThest/ViewController.swift
+++ GitBrancThest/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+let number = 5
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
